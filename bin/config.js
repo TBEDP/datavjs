ed5da90667680dd9dc37a3355b59bd1bc6c3aa97
Jackson Tian <shyvo1987@gmail.com>
2012-10-29 02:01:05 +0800

    Add build script

---
+++ bin/config.js
@@ -0,0 +1,10 @@
+exports.datav = [
+  'deps/array_shim.js',
+  'deps/json2.js',
+  'deps/d3.js',
+  'deps/raphael.js',
+  'deps/eventproxy.js',
+  'deps/jquery-1.7.1.js',
+  'deps/underscore-1.4.2.js',
+  'lib/datav.js'
+];
